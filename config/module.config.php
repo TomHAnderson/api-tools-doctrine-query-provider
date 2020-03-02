3853e64f55e1497c94e7f33c2cceddd6ca068908
Tom H Anderson <tom.h.anderson@gmail.com>
2020-03-02 15:45:14 -0700

    Config file

---
+++ config/module.config.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Api\Tools\Doctrine\QueryProvider;
+
+return [
+    'api-tools-doctrine-query-provider' => [
+        'abstract_factories' => [
+            Service\QueryProvider\ORM\AbstractFactory::class,
+            Service\QueryProvider\ODM\AbstractFactory::class,
+        ],
+    ],
+    'api-tools-doctrine-query-create-filter' => [
+        'abstract_factories' => [
+            Service\QueryCreateFilter\ORM\AbstractFactory::class,
+            Service\QueryCreateFilter\ODM\AbstractFactory::class,
+        ],
+    ],
+];
